b9be3ae40f723be234ff40455dd8370fb52b0e18
corhythm <dnr2144@gmail.com>
2022-05-15 22:38:38 +0900

    [STYLE] Delete print log code

--- src/main/kotlin/BOJ2750.kt
+++ src/main/kotlin/BOJ2750.kt
@@ -10,26 +10,28 @@ class BOJ2750 {
 
         val numOfInput = nextInt()
         val unsortedNums = IntArray(numOfInput)
+
         for (i in 0 until numOfInput) {
             val value = nextInt()
             unsortedNums[i] = value
         }
 
+        bubbleSort(unsortedNums)
+
+        unsortedNums.iterator().forEach {
+            println(it)
+        }
+    }
+
+    private fun bubbleSort(unsortedNums: IntArray) {
         for (i in unsortedNums.indices) {
             for (j in  0  until (unsortedNums.size - i - 1)) {
                 if (unsortedNums[j + 1] < unsortedNums[j]) {
-//                    println("${unsortedNums[j + 1]} < ${unsortedNums[j]}")
                     val temp = unsortedNums[j + 1]
                     unsortedNums[j + 1] = unsortedNums[j]
                     unsortedNums[j] = temp
                 }
-//                println("$i, ${unsortedNums.toList()}")
             }
-        }
-
-//        println("sorted Array")
-        unsortedNums.iterator().forEach {
-            println(it)
         }
     }
 

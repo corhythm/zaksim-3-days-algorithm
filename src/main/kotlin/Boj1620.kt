b21fdc35205d97d4b4e855a5aa0cfeb51b972d4b
corhythm <dnr2144@gmail.com>
2022-05-26 00:21:45 +0900

    [Solve] Solve Boj1620, 나는야 포켓몬 마스터 이다솜(Sliver 4)

---
+++ src/main/kotlin/Boj1620.kt
@@ -0,0 +1,28 @@
+import java.util.Scanner
+
+class Boj1620 {
+
+    fun run() = with(Scanner(System.`in`)) {
+        val pokemonNum = nextInt()
+        val questionNum = nextInt()
+        val pokemons = linkedMapOf<String, Int>()
+
+        for (i in 1 .. pokemonNum) {
+            pokemons[next()] = i
+        }
+        // key -> value, value -> key
+        val pokemonsReverse = pokemons.entries.associate { (k, v) -> v to k }
+        println(pokemonsReverse)
+
+        for (i in 0 until questionNum) {
+            val value = next()
+            try {
+                println(pokemonsReverse[value.toInt()])
+            } catch (ex: java.lang.NumberFormatException) { // string일 때
+                println(pokemons[value])
+            }
+        }
+    }
+}
+
+fun main() = Boj1620().run()

1b662bef38197276c9de12d793c01ea07fb8aaf5
corhythm <dnr2144@gmail.com>
2022-06-03 00:50:38 +0900

    [Solve] Solve Boj 1037, 약수(Sliver 5)

---
+++ src/main/kotlin/Boj1037.kt
@@ -0,0 +1,24 @@
+import java.util.StringTokenizer
+import kotlin.math.pow
+
+class Boj1037 {
+
+    fun solve() {
+        val divisorNum = readln().toInt() // 약수 개수
+        val divisors = arrayListOf<Int>()
+
+        val st = StringTokenizer(readln())
+        repeat(divisorNum) {
+            divisors.add(st.nextToken().toInt())
+        }
+        divisors.sort()
+
+        if (divisors.size == 1) {
+            println(divisors.first().toDouble().pow(2).toInt())
+        } else {
+            println(divisors.first().times(divisors.last()))
+        }
+    }
+}
+
+fun main() = Boj1037().solve()
